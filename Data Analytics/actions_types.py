a190165cc5ad45299d2aad683f5f127bb4c39117
RaxDev <blackfortdevops@gmail.com>
2022-11-22 14:57:12 +0530

    Made Graph plot for the data

--- Data Analytics/actions_types.py
+++ Data Analytics/actions_types.py
@@ -11,6 +11,77 @@ for i in data:
     except:
         crimes[i["category"]] = 1
 
+"""
 for i in crimes:
     print(i,":",crimes[i])
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+mylabels = []
+count = []
+
+for i in crimes:
+    mylabels.append(i)
+    count.append(crimes[i])
+
+"""
+myexplode = []
+z=0.1
+odev = 0
+
+for i in crimes:
+    mylabels.append(i)
+    count.append(crimes[i])
+    if(odev%2 == 0):
+        myexplode.append(z)
+    else:
+        myexplode.append(0)
+    odev += 1
+    #print(i,":",crimes[i])
+"""
+
+#"""
+l = len(mylabels)
+for i in range(l):
+    for j in range(i+1,l):
+        if(count[i]>count[j]):
+            temp = count[i]
+            count[i] = count[j]
+            count[j] = temp
+
+            temp2 = mylabels[i]
+            mylabels[i] = mylabels[j]
+            mylabels[j] = temp2
+
+[print(mylabels[i],count[i]) for i in range(l)]
+#"""
+myexplode = [0 for x in range(l)]
+myexplode[-1] = 0.15
+
+disp_cnt = 0
+if(l<5):
+    disp_cnt = l
+else:
+    disp_cnt = 5
+
+#"""
+for i in range(l):
+    mylabels[i] = mylabels[i].upper() +" : "+ str(count[i])
+#"""
+
+#"""
+count = count[::-1]
+mylabels = mylabels[::-1]
+myexplode = myexplode[::-1]
+#"""
+plt.rcParams.update({'font.size': 18}) #Change font size of every text in plot
+plt.pie(count[0:disp_cnt+1], labels = mylabels[0:disp_cnt+1], explode=myexplode[0:disp_cnt+1], startangle = 0, autopct="%0.2f%%", shadow = True)
+
+#print(mylabels[0:disp_cnt+1])
+#plt.pie(count, labels = mylabels) #No displacement
+#plt.pie(count, labels = mylabels, explode=myexplode, startangle = 0) #, shadow = True) #incremental displacement
+#plt.legend()
+plt.show()
 #print(crimes)
