30b6c323ded5cb386421ff717c8b29a917071b8d
alanmohan <alanmohan86@gmail.com>
2022-11-25 19:17:09 +0400

    final changes to php script

--- Postman_v0.3/crime_analyse.php
+++ Postman_v0.3/crime_analyse.php
@@ -1,33 +1,43 @@
 <?php
 // error_reporting(E_ALL);
 $file='./crime_incidents.json';
-
-$json = file_get_contents($file);
-// echo 'json'.$json;
-// die;
-$json_data = json_decode($json, true);
-// print_r($json_data);
-$crime_data=array();
-$crime_types=array();
-foreach($json_data as $key => $value)
+session_start();
+if(strcmp($_SESSION['city_name'],'london')==0)
 {
-    $category=$value['category'];
-    // print_r($value);
-    array_push($crime_types,$category);
+    $json = file_get_contents(base_url().'/lon_rep.json');
+    $json_file = fopen('./crime_report.json','w') or die('unable to write into file');
+    fwrite($json_file,$json);
+    fclose($json_file);
+    // copy((base_url().'/lon_rep.json'),$file);
+    header('Location:'.base_url().'/heatMap.html');
 }
-$crime_data=array_fill_keys($crime_types,0);
-foreach($json_data as $key => $value)
-{
-    $category=$value['category'];
-    $crime_data[$category]++;
+else{
+    $json = file_get_contents($file);
+    // echo 'json2'.$json;
+    // die;
+    $json_data = json_decode($json, true);
+    // print_r($json_data);
+    $crime_data=array();
+    $crime_types=array();
+    foreach($json_data as $key => $value)
+    {
+        $category=$value['category'];
+        // print_r($value);
+        array_push($crime_types,$category);
+    }
+    $crime_data=array_fill_keys($crime_types,0);
+    foreach($json_data as $key => $value)
+    {
+        $category=$value['category'];
+        $crime_data[$category]++;
+    }
+    arsort($crime_data);
+    // print_r($crime_data);
+    $myjson = fopen("./crime_report.json", "w") or die("Unable to open file!");
+    fwrite($myjson,json_encode($crime_data));
+    fclose($myjson);
+    header('Location:'.base_url().'/heatMap.html');
 }
-arsort($crime_data);
-// print_r($crime_data);
-$myjson = fopen("./crime_report.json", "w") or die("Unable to open file!");
-fwrite($myjson,json_encode($crime_data));
-fclose($myjson);
-header('Location:'.base_url().'/Postman_v0.3/heatMap.html');
-
 //returns base url of the server
 function base_url(){
     if(isset($_SERVER['HTTPS'])){
